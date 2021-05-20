291c72519317c120e61d9b1f8ce209c24429b39d
Evgeniya Popova <evgeniya.popova@wdg.com.ua>
2021-05-20 16:48:23 +0300

    created animation

--- src/assets/js/animation.js
+++ src/assets/js/animation.js
@@ -6,10 +6,65 @@ $(function () {
             innerCard = $('.filter__card-item-inner'),
             btnCloseItem = $('.filter__info-close-btn');
 
-            
-            card.each( function () {
+            card.each( function (i) {
+              $(this).attr('id', i);
+              $(this).css('order', i);
               feedback.hide();
-                $(this).on('click' , function () { 
+
+                $(this).on('click' , function () {
+                    card.each( function (i) {
+                        $(this).css('order', i);
+                    })
+
+                    if ($(window).width() >= 768) {
+                        if($(this).attr('id') == card.length-1) {
+                            let orderThis = $(this).css('order');
+                            if ( $(window).width() < 1200) {
+                                $(this).css('order', orderThis - 2);
+                            }
+                            if ( $(window).width() >= 1200) {
+                                $(this).css('order', orderThis - 5);
+                            }
+                        }
+                        if ( $(window).width() < 1200) {
+                            switch ($(this).attr('id') % 2) {
+                                case 1: {
+                                    let orderNext = $(this).next().css('order')
+                                    $(this).next().css('order', orderNext - 2);
+                                    break;
+                                }
+                                case 0: {
+                                    let orderPrev = $(this).prev().css('order')
+                                    if (orderPrev !== undefined) {
+                                        $(this).prev().css('order', orderPrev - 2);
+                                    } else {
+                                        if ($(this).attr('id') === 0) {
+                                            let orderThis = $(this).css('order');
+                                            $(this).css('order', orderThis - 2);
+                                        } else {
+                                            let orderNext = $('#0').css('order');
+                                            $('#0').css('order', orderNext - 1);
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                        if ( $(window).width() >= 1200) {
+                            if ($(this).attr('id') <= 4 ) {
+                                $(this).css('order', 6);
+                            }
+                            if ($(this).attr('id') > 5 && $(this).attr('id') <= 9 ) {
+                                $(this).css('order', 11);
+                            }
+                            if ($(this).attr('id') > 10 && $(this).attr('id') <= 14 ) {
+                                $(this).css('order', 16);
+                            }
+                            if ($(this).attr('id') >= 15 && $(this).attr('id') < 20 ) {
+                                $(this).css('order', 14);
+                            }
+                        }
+                    }
                     card.removeClass('active');
                     feedback.hide();
                     card.find('.filter__card-item-inner').show();
@@ -18,7 +73,7 @@ $(function () {
                       $(this).addClass('active');
                       $(this).children('.filter__card-item-inner').hide();
                       $(this).children('.filter__card-feedback').show();
-                      console.log('rrt');
+                      //console.log('rrt');
                       
                       return;
                     }                     
