51c4e29011020feaa7ecf1cb6cd705571463da30
Max <max.s@newbirddesign.com>
2021-09-01 11:59:39 +0300

    fix js

--- src/assets/js/jQuery.js
+++ src/assets/js/jQuery.js
@@ -11,182 +11,197 @@ $(function () {
     fakeDropInBody: false,
   });
 
-  var typed = new Typed("#typed-entry", {
-    strings: ["Because EVERY", "BUSINESS", "<p>NEEDS SOMETHING</p>"],
-    typeSpeed: 20,
-    backSpeed: 20,
-    startDelay: 200,
-    backDelay: 1200,
-  });
+  function typed() {
+    let pereloaderTypedText = $(".preloader");
 
+    if (pereloaderTypedText.length > 0) {
+      var typed = new Typed("#typed-entry", {
+        strings: ["Because EVERY", "BUSINESS", "<p>NEEDS SOMETHING</p>"],
+        typeSpeed: 20,
+        backSpeed: 20,
+        startDelay: 200,
+        backDelay: 1200,
+      });
+    }
+  }
+
+  typed();
 
   function masonry() {
-    var masonryItems = $(".filter__card-item"),
+    let starter = $("#filter");
+
+    if (starter.length > 0) {
+      var masonryItems = $(".filter__card-item"),
         feedback = $(".filter__card-feedback"),
         width = 388;
 
-    feedback.hide();
-    
-    
-    masonryItems.each(function (i) {
-      $(this).attr("data-card", i++);
-    });
+      feedback.hide();
 
-    let masonryLauout = $(".filter__card").masonry({
-      itemSelector: ".filter__card-item",
-      columnWidth: width,
-      gutter: 22,
-      // fitWidth: true
-    });
-  
-    // nodes in row, can be changed on resize action if needed
-    // return 1 for disabling change node position
-    function getNodesInRow(){
-
-
-      if ($(window).width() <= 1106) {
-        console.log('767');
-        return 1
-      } 
-
-      if ($(window).width() <= 1436) {
-        console.log('1354');
-        return 2
-      } 
-
-      if($(window).width() <= 1766){
-        return 3
-      } else {
-        return 4
-      }
-      
-    }
-
-    function insertNode(elem) {
-      const nodesInRow = getNodesInRow();
-      const currentIndex = parseInt(elem.attr("data-card"));// index clicked card
-      const activeNode = $(`[data-active='true']`);
-      
-      if(activeNode.length){
-        resetPositionsByIndex(activeNode)
-      }
-      // if first in row, don't change element position
-      if(!Number.isInteger(currentIndex / nodesInRow) ){
-        const removedNode = $(elem).detach()
-        const newBeforeIndex = Math.floor(currentIndex/nodesInRow) * nodesInRow;
-        const newPreviewsElem = $(`[data-card='${newBeforeIndex}']`);
-        removedNode.attr('data-active', 'true');
-        removedNode.insertBefore(newPreviewsElem);
-      }
-    }
-
-    function resetPositionsByIndex(node){
-      const nodesInRow = getNodesInRow();
-      const currentIndex = parseInt(node.attr("data-card"));
-
-      // if first in row, don't change element position
-      if(!Number.isInteger(currentIndex / nodesInRow) ) {
-        node.removeAttr("data-active");
-        const removedNode = $(node).detach()
-        const newPreviewsElem = $(`[data-card='${currentIndex - 1}']`)
-        removedNode.insertAfter(newPreviewsElem);
-      }
-
-    }
-
-    masonryItems.each(function () {
-      let fruitCount = $(this).attr("data-card");
-      // $(this)
-      //   .find(".filter__card-item-inner")
-      //   .append("<p> " + fruitCount + "</p>");
-      $(this).on("click", function () {
-        console.log('click function masonry() ITM');
-        insertNode($(this))
-        masonryLauout.masonry("reloadItems");
-        masonryLauout.masonry(masonryItems);
+      masonryItems.each(function (i) {
+        $(this).attr("data-card", i++);
       });
 
-    });
-
-    // reorganization of elements when clicking on BTN close
-
-    $(".filter__info-close-btn").each(function () {
-      $(this).on("click", function () {
-        console.log('click function masonry() BTN CLOSE');
-        resetPositionsByIndex($(this).closest('.filter__card-item'));
-        masonryLauout.masonry("reloadItems");
-        masonryLauout.masonry(masonryItems);
+      let masonryLauout = $(".filter__card").masonry({
+        itemSelector: ".filter__card-item",
+        columnWidth: width,
+        gutter: 22,
+        // fitWidth: true
       });
-    });
+
+      // nodes in row, can be changed on resize action if needed
+      // return 1 for disabling change node position
+      function getNodesInRow() {
+        if ($(window).width() <= 1106) {
+          console.log("767");
+          return 1;
+        }
+
+        if ($(window).width() <= 1436) {
+          console.log("1354");
+          return 2;
+        }
+
+        if ($(window).width() <= 1766) {
+          return 3;
+        } else {
+          return 4;
+        }
+      }
+
+      function insertNode(elem) {
+        const nodesInRow = getNodesInRow();
+        const currentIndex = parseInt(elem.attr("data-card")); // index clicked card
+        const activeNode = $(`[data-active='true']`);
+
+        if (activeNode.length) {
+          resetPositionsByIndex(activeNode);
+        }
+        // if first in row, don't change element position
+        if (!Number.isInteger(currentIndex / nodesInRow)) {
+          const removedNode = $(elem).detach();
+          const newBeforeIndex =
+            Math.floor(currentIndex / nodesInRow) * nodesInRow;
+          const newPreviewsElem = $(`[data-card='${newBeforeIndex}']`);
+          removedNode.attr("data-active", "true");
+          removedNode.insertBefore(newPreviewsElem);
+        }
+      }
+
+      function resetPositionsByIndex(node) {
+        const nodesInRow = getNodesInRow();
+        const currentIndex = parseInt(node.attr("data-card"));
+
+        // if first in row, don't change element position
+        if (!Number.isInteger(currentIndex / nodesInRow)) {
+          node.removeAttr("data-active");
+          const removedNode = $(node).detach();
+          const newPreviewsElem = $(`[data-card='${currentIndex - 1}']`);
+          removedNode.insertAfter(newPreviewsElem);
+        }
+      }
+
+      masonryItems.each(function () {
+        let fruitCount = $(this).attr("data-card");
+        // $(this)
+        //   .find(".filter__card-item-inner")
+        //   .append("<p> " + fruitCount + "</p>");
+        $(this).on("click", function () {
+          console.log("click function masonry() ITM");
+          insertNode($(this));
+          masonryLauout.masonry("reloadItems");
+          masonryLauout.masonry(masonryItems);
+        });
+      });
+
+      // reorganization of elements when clicking on BTN close
+
+      $(".filter__info-close-btn").each(function () {
+        $(this).on("click", function () {
+          console.log("click function masonry() BTN CLOSE");
+          resetPositionsByIndex($(this).closest(".filter__card-item"));
+          masonryLauout.masonry("reloadItems");
+          masonryLauout.masonry(masonryItems);
+        });
+      });
     }
+  }
 
   masonry();
 
   function dropdownHeader() {
-    let dropBtn = $('.header__checkin'),
-        list = $('.dropdown ul');
-        container = $('.dropdown')
+    let dropBtn = $(".header__checkin"),
+      list = $(".dropdown ul");
+    container = $(".dropdown");
 
-        dropBtn.on('click' , function (e) {
-            e.preventDefault();
-            
-            if (!container.is('.active')) {
-                container.addClass('active');
-                list.slideDown(400);
-                console.log('open');
-                
-            } else {
-              list.slideUp(400);
-              setTimeout(() => { 
-                container.removeClass('active');
-              }, 400); 
-              console.log('close');
-              
-            }
+    dropBtn.on("click", function (e) {
+      e.preventDefault();
 
-        })
+      if (!container.is(".active")) {
+        container.addClass("active");
+        list.slideDown(400);
+        console.log("open");
+      } else {
+        list.slideUp(400);
+        setTimeout(() => {
+          container.removeClass("active");
+        }, 400);
+        console.log("close");
+      }
+    });
 
-        $(document).on("click", function (e) {
-          
-          if (
-            !dropBtn.is(e.target) &&
-            dropBtn.has(e.target).length === 0 &&
-            !list.is(e.target) &&
-            list.has(e.target).length === 0
-            ) {
-              list.slideUp(400); 
-              setTimeout(() => { 
-                container.removeClass('active');
-              }, 400);              
-            }
-          });
+    $(document).on("click", function (e) {
+      if (
+        !dropBtn.is(e.target) &&
+        dropBtn.has(e.target).length === 0 &&
+        !list.is(e.target) &&
+        list.has(e.target).length === 0
+      ) {
+        list.slideUp(400);
+        setTimeout(() => {
+          container.removeClass("active");
+        }, 400);
+      }
+    });
   }
 
   dropdownHeader();
 
   function muteSocialLink() {
-      let link = $('.footer__social a');
+    let link = $(".footer__social a");
 
-      link.on('click' , function (event) {
-        event.preventDefault();
-      })
+    link.on("click", function (event) {
+      event.preventDefault();
+    });
   }
 
   muteSocialLink();
 
   function checkboxDisabled() {
-    let checkbox = $('#edit-field-my-kategorie .input-group__checkbox input:checkbox');
-    let checkboxWrap = $('#edit-field-my-kategorie .input-group__clarification')
-    $(this).on('click' , function () {
-    const myCount =  $('#edit-field-my-kategorie .input-group__checkbox input:checkbox:checked').length;
-      checkbox.each( function () {
-        if (myCount >=  3) {
-          checkbox.not(':checked').prop('disabled', true).closest(checkboxWrap).addClass('disabled')
-          } else {
-            checkbox.prop('disabled', false).closest(checkboxWrap).removeClass('disabled')
-          }
-      })
-    })
+    let checkbox = $(
+      "#edit-field-my-kategorie .input-group__checkbox input:checkbox"
+    );
+    let checkboxWrap = $(
+      "#edit-field-my-kategorie .input-group__clarification"
+    );
+    $(this).on("click", function () {
+      const myCount = $(
+        "#edit-field-my-kategorie .input-group__checkbox input:checkbox:checked"
+      ).length;
+      checkbox.each(function () {
+        if (myCount >= 3) {
+          checkbox
+            .not(":checked")
+            .prop("disabled", true)
+            .closest(checkboxWrap)
+            .addClass("disabled");
+        } else {
+          checkbox
+            .prop("disabled", false)
+            .closest(checkboxWrap)
+            .removeClass("disabled");
+        }
+      });
+    });
   }
 
   checkboxDisabled();
